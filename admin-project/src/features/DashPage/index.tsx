c3955c0cd1aabce12cf5f9534413f1945cdf09ea
ounjuu <koj1555@naver.com>
2025-04-16 20:33:33 +0900

    Feat: dashpage

--- admin-project/src/features/DashPage/index.tsx
+++ admin-project/src/features/DashPage/index.tsx
@@ -1,13 +1,43 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 import TitleCompo from "@/components/TitleCompo";
 import { DashStyled } from "./styled";
-import PrivateRoute from "@/components/PrivateRoute";
+
 const DashBoard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAdminLogin = async () => {
+      try {
+        // 서버에서 로그인 상태 확인
+        const response = await axios.get(
+          "http://localhost:5000/auth/adminCheck",
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (response.data.isLoggedIn) {
+          console.log("User is authenticated");
+        } else {
+          console.log("User is not authenticated, redirecting to login");
+          router.push("/login"); // 로그인 페이지로 리다이렉트
+        }
+      } catch (error) {
+        console.log("Error during authentication check", error);
+        router.push("/login"); // 로그인 페이지로 리다이렉트
+      }
+    };
+
+    checkAdminLogin();
+  }, [router]);
+
   return (
-    <PrivateRoute>
-      <DashStyled>
-        <TitleCompo title="대시보드" />
-      </DashStyled>
-    </PrivateRoute>
+    <DashStyled>
+      <TitleCompo title="대시보드" />
+    </DashStyled>
   );
 };
+
 export default DashBoard;
